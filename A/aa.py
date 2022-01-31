0a9c595680c12b2a42f3c7741a94dcb8b3563df4
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-01-31 13:20:36 -0800

    Update aa.py

--- A/aa.py
+++ A/aa.py
@@ -1,19 +1,19 @@
 import sqlite3
-
+                                        #connect to test.db
 conn = sqlite3.connect('test.db')
 with conn:
-    cur = conn.cursor()
+    cur = conn.cursor()                             #create tbl_files 
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_files(ID INTEGER PRIMARY KEY AUTOINCREMENT,\
                 col_name TEXT, col_type TEXT)")
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_string(col_string VARCHAR(255))")
-    conn.commit()
+    conn.commit()                                           #create tbl_string
 conn = sqlite3.connect('test.db')
 
-files = ('information.docx','hello.txt','myimage.phg', \
+files = ('information.docx','hello.txt','myimage.phg', \        #giles to sort
                  'mymovie.mpg','world.txt','data.pdf','myphoto.jpg')
 
 for x in files:
-    if x.endswith('txt'):
+    if x.endswith('txt'):               #insert only txt files into table
         with conn:
             cur = conn.cursor()
             cur.execute("INSERT INTO tbl_files (col_name) VALUES (?)", (x,))
