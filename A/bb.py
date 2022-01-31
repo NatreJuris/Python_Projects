97b43acbf8e8686645fd4d8567addd8805aa574c
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-01-31 09:11:16 -0800

    Add files via upload

---
+++ A/bb.py
@@ -0,0 +1,16 @@
+from sre_constants import OP_LOCALE_IGNORE
+
+
+class River:
+    substance = 'water'
+    current_flow = 'downstream'
+    destination = 'ocean'
+    source = 'clouds'
+
+class Columbia(River):
+    location = 'Oregon'
+    size = 'large'
+
+class Nile(River):
+    location = 'Egypt'
+    size = 'large'
