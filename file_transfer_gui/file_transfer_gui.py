422f34b153e99976920439107a1eb96710f6ca5e
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-02-14 11:18:05 -0800

    Add files via upload

--- file_transfer_gui/file_transfer_gui.py
+++ file_transfer_gui/file_transfer_gui.py
@@ -1,55 +1,26 @@
 import time
 import os
 import shutil
-import pathlib
 from tkinter import filedialog
 from tkinter import *
 
 
-win = Tk()
-b1 = Button(win, text="Source Directory", bg='orange')      #gui layout
-b2 = Button(win, text="Destination Directory", bg='green')
-b3 = Button(win, text="Start File Transfer", bg='yellow')
-b1.grid(row=0, column=0)
-b2.grid(row=3, column=0)
-b3.grid(row=5, column=0)
-
 def but1():                     #button 1
-    but1 = b1
-    global folder_path1
     filename1 = filedialog.askdirectory()
-    folder_path1.set(filename1)
-    print(filename1)
-
-folder_path1 = StringVar()
-lbl1 = Label(win,textvariable=folder_path1)
-lbl1.grid(row=0, column=3)
-         
-
-b1.configure(command=but1)
+    lbl1.delete(0, END)
+    lbl1.insert(0, filename1)
 
 def but2():                     #buttton 2
-    but2 = b2
-    global folder_path2
     filename2 = filedialog.askdirectory()
-    folder_path2.set(filename2)
-    print(filename2)
-
-folder_path2 = StringVar()
-lbl2 = Label(win,textvariable=folder_path2)
-lbl2.grid(row=3, column=3)
-
-        
-
-b2.configure(command=but2)
+    lbl2.delete(0, END)
+    lbl2.insert(0, filename2)
 
 def but3():                     #button 3
     but3 = b3
-
     SECONDS_IN_DAY = 24 * 60 * 60           #24 hour count
 
-    src = folder_path1
-    dst = folder_path2
+    src = lbl1.get()
+    dst = lbl2.get()
 
     now = time.time()
     before = now - SECONDS_IN_DAY
@@ -57,14 +28,28 @@ def but3():                     #button 3
     def last_mod_time(fname):
         return os.path.getmtime(fname)
 
-    for fname in os.listdir():
+    for fname in os.listdir(src):
         src_fname = os.path.join(src, fname)
-        if (last_mod_time(src_fname) > before):
+        if (last_mod_time(src_fname) < before):
             dst_fname = os.path.join(dst, fname)
             shutil.move(src_fname, dst_fname)
             print("File Transfer Started")
 
-b3.configure(command=but3)
+win = Tk()
+b1 = Button(win, text="Source Directory", bg='orange', command=but1)      #gui layout
+b2 = Button(win, text="Destination Directory", bg='green', command=but2)
+b3 = Button(win, text="Start File Transfer", bg='yellow', command=but3)
+
+b1.grid(row=0, column=0)
+b2.grid(row=3, column=0)
+b3.grid(row=5, column=0)
+
+lbl1 = Entry(win, width=100)
+lbl1.grid(row=0, column=3)
+
+lbl2 = Entry(win, width=100)
+lbl2.grid(row=3, column=3)
+
 
 win.mainloop()
 
