e4d0c952f338b8ead51b9ac3eb2af2f2a0067e7f
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-02-20 13:26:28 -0800

    Delete Classes directory

--- Classes/Classes/classApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Course Number, Duration, Instructor Name, Title
-
-admin.site.register(Title)
-admin.site.register(Course Number)
-admin.site.register(Instructor Name)
-admin.site.register(Duration)
